6909ac01f6ce78faac755eca4e4885bf89608b8d
Tanmay Mondal <tanmoy10122000@gmail.com>
2025-10-03 20:53:16 +0530

    ..

--- my-app/src/app/layout.tsx
+++ my-app/src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "sonner";
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import AppSidebar from "@/components/app-sidebar"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,11 +27,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-        <Toaster richColors />
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <SidebarProvider>
+          <div className="flex min-h-screen w-full">
+            <AppSidebar />
+
+            <main className="flex-1">
+              <SidebarTrigger />
+              {children}
+              <Toaster richColors />
+            </main>
+
+          </div>
+        </SidebarProvider>
+
       </body>
     </html>
   );
